e3910b288b95ba80ecf898bfc7fb6cbeef63c0f5
konstantinosKokos <konstantinos@riseup.net>
2020-06-28 09:26:20 +0200

    add greedy

--- Transformers/transformer.py
+++ Transformers/transformer.py
@@ -3,7 +3,7 @@ from Transformers.encoder import make_encoder
 from Transformers.decoder import make_decoder
 from Transformers.embedding import InvertibleEmbedder
 
-from torch import Tensor, LongTensor
+from torch import Tensor, LongTensor, triu, ones, empty, cat
 
 from typing import NoReturn
 
@@ -38,3 +38,42 @@ class Transformer(Module):
         encoded = self.encoder((encoder_embeddings, encoder_mask))[0]
         decoded = self.decoder((encoded, encoder_mask, decoder_embeddings, decoder_mask))[2]
         return self.out_embedder.invert(decoded)
+
+    def forward_greedy(self, encoder_idxes: LongTensor, encoder_mask: LongTensor, max_decode_length: int,
+                       sos_id: int) -> LongTensor:
+        self.eval()
+        device = encoder_idxes.device
+
+        b, s_in = encoder_idxes.shape
+        s_out = max_decode_length
+
+        encoder_embeddings = self.inp_embedder.embed(encoder_idxes)
+        causal_mask = self._make_causal_mask(b, s_out).to(device)
+        decoder_mask = self._make_encdec_mask(encoder_mask, s_out)
+
+        encoded = self.encoder((encoder_embeddings, encoder_mask))
+
+        output_symbols = (ones(b) * sos_id).long().to(device)
+        decoder_input = (self.out_embedder.embed(output_symbols)).unsqueeze(1)
+        output_symbols = output_symbols.unsqueeze(1)
+        decoder_output = empty(b, s_out, encoder_embeddings.shape[2]).to(device)
+
+        for t in range(s_out):
+            _decoder_tuple_input = (encoded, decoder_mask,
+                                    decoder_input, causal_mask[:, :t + 1, :t + 1])
+            repr_t = self.decoder(_decoder_tuple_input)[2][:, -1]
+            weights_t = self.out_embedder.invert(repr_t)
+            class_t = weights_t.argmax(dim=-1)
+            output_symbols = cat([output_symbols, class_t.unsqueeze(1)], dim=1)
+            next_embedding = self.out_embedder.embed(class_t).unsqueeze(1)
+            decoder_input = cat([decoder_input, next_embedding], dim=1)
+            decoder_output[:, t] = repr_t
+        return output_symbols
+
+    def _make_encdec_mask(self, encoder_mask: LongTensor, max_dec_len: int) -> LongTensor:
+        return encoder_mask[:, 0: 1, :].repeat(1, max_dec_len, 1)
+
+    @staticmethod
+    def _make_causal_mask(b: int, n: int) -> LongTensor:
+        upper_triangular = triu(ones(b, n, n), diagonal=1)
+        return ones(b, n, n) - upper_triangular
